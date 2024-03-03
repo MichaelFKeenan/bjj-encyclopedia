5c58eb6fce29e469fadb5e09229339bb115e7b2a
michael keenan <mikemk309@googlemail.com>
2024-03-03 21:02:22 +0000

    add amplify hosting api

--- bjj-encyclopedia-api/bin/postbuild.sh
+++ bjj-encyclopedia-api/bin/postbuild.sh
@@ -7,6 +7,4 @@ mkdir -p ./.amplify-hosting/compute
 cp -r ./dist ./.amplify-hosting/compute/default
 cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
 
-cp -r public ./.amplify-hosting/static
-
 cp deploy-manifest.json ./.amplify-hosting/deploy-manifest.json
